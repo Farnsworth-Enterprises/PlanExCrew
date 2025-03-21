247196555d14cff2418cc0e91d71df084bf5d30d
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2025-03-21 13:45:04 -0400

    Added Game routing, randomMisson logic

--- src/components/About.js
+++ src/components/About.js
@@ -2,95 +2,99 @@ import React from "react";
 import { Box, Typography, Link, Paper } from "@mui/material";
 
 const About = () => {
-  return (
-    <Box
-      sx={{
-        width: "100vw",
-        height: "100vh",
-        backgroundImage: `url("https://images.alphacoders.com/236/236781.jpg")`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "flex-end", 
-      }}
-    >
-    <Typography
-        variant="h1"
-        sx={{
-          position: "absolute",
-          top: "10%",
-          left: "50%",
-          transform: "translateX(-50%)",
-          color: "white",
-          fontWeight: "bold",
-          textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
-        }}
-      >
-        About
-      </Typography>
-      <Paper
-        elevation={6} 
-        sx={{
-          padding: "20px",
-          marginRight: "10%",
-          maxWidth: "400px",
-          backgroundColor: "rgba(255, 255, 255, 0.9)", 
-          color: "black",
-          borderRadius: "10px",
-        }}
-      >
+	return (
+		<Box
+			sx={{
+				width: "100vw",
+				height: "100vh",
+				backgroundImage: `url("https://images.alphacoders.com/236/236781.jpg")`,
+				backgroundSize: "cover",
+				backgroundPosition: "center",
+				backgroundRepeat: "no-repeat",
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "flex-end",
+			}}
+		>
+			<Typography
+				variant="h1"
+				sx={{
+					position: "absolute",
+					top: "10%",
+					left: "50%",
+					transform: "translateX(-50%)",
+					color: "white",
+					fontWeight: "bold",
+					textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+				}}
+			>
+				About
+			</Typography>
+			<Paper
+				elevation={6}
+				sx={{
+					padding: "20px",
+					marginRight: "10%",
+					maxWidth: "400px",
+					backgroundColor: "rgba(255, 255, 255, 0.9)",
+					color: "black",
+					borderRadius: "10px",
+				}}
+			>
+				<Typography variant="h4" gutterBottom>
+					How to Play PlanExCrew
+				</Typography>
+				<Typography variant="body1" sx={{ marginBottom: "20px" }}>
+					From a list of potential crew members, choose your crew of
+					at most 3 to tackle the mission presented to you based on a
+					difficulty score. Each crew member will have a rating to
+					help the user chose who best to chose for the mission. After
+					selection, the user will receive a mission result based on
+					the difficulty of the mission and the competency total of
+					the chosen crew members.
+				</Typography>
 
-        <Typography variant="h4" gutterBottom>
-          How to Play PlanExCrew
-        </Typography>
-        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
-        From a list of potential crew members, choose your crew of at most 3 to tackle the mission presented to you based on a difficulty score. Each crew member will have a rating to help the user chose who best to chose for the mission. After selection, the user will receive a mission result based on the difficulty of the mission and the competency total of the chosen crew members.
-        </Typography>
+				<Typography variant="h5" gutterBottom>
+					Project Repository
+				</Typography>
+				<Link
+					href="https://github.com/Farnsworth-Enterprises/PlanExCrew"
+					target="_blank"
+					rel="noopener"
+					color="primary"
+					underline="hover"
+					variant="h6"
+				>
+					PlanExCrew GitHub Repository
+				</Link>
 
-        <Typography variant="h5" gutterBottom>
-          Project Repository
-        </Typography>
-        <Link
-          href="https://github.com/Farnsworth-Enterprises/PlanExCrew"
-          target="_blank"
-          rel="noopener"
-          color="primary"
-          underline="hover"
-          variant="h6"
-        >
-          PlanExCrew GitHub Repository
-        </Link>
-
-        <Typography variant="h5" sx={{ marginTop: "20px" }}>
-          Created by
-        </Typography>
-        <Link
-          href="https://github.com/jbiehl88"
-          target="_blank"
-          rel="noopener"
-          color="primary"
-          underline="hover"
-          variant="h6"
-        >
-          Jordan Biehl
-        </Link>
-        <br />
-        <Link
-          href="https://github.com/zluigon"
-          target="_blank"
-          rel="noopener"
-          color="primary"
-          underline="hover"
-          variant="h6"
-        >
-          Luis Gonzalez
-        </Link>
-      </Paper>
-    </Box>
-  );
+				<Typography variant="h5" sx={{ marginTop: "20px" }}>
+					Created by
+				</Typography>
+				<Link
+					href="https://github.com/jbiehl88"
+					target="_blank"
+					rel="noopener"
+					color="primary"
+					underline="hover"
+					variant="h6"
+				>
+					Jordan Biehl
+				</Link>
+				<br />
+				<Link
+					href="https://github.com/zluigon"
+					target="_blank"
+					rel="noopener"
+					color="primary"
+					underline="hover"
+					variant="h6"
+				>
+					Luis Gonzalez
+				</Link>
+			</Paper>
+		</Box>
+	);
 };
 
 export default About;
-
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,43 +1,74 @@
 import React, { useState } from "react";
-import { AppBar, Toolbar, Typography, IconButton, Drawer, List, ListItem, ListItemText } from "@mui/material";
+import {
+	AppBar,
+	Toolbar,
+	Typography,
+	IconButton,
+	Drawer,
+	List,
+	ListItem,
+	ListItemText,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(false);
 
-  const toggleDrawer = (state) => () => {
-    setOpen(state);
-  };
+	const toggleDrawer = (state) => () => {
+		setOpen(state);
+	};
 
-  return (
-    <AppBar position="fixed" sx={{ backgroundColor: "#7FFFD4", color: "#000000"}}>
-      <Toolbar>
-        <Typography
-          variant="h6"
-          component={Link}
-          to="/"
-          sx={{ flexGrow: 1, textDecoration: "none", color: "black", fontWeight: "bold" }}
-        >
-          Home
-        </Typography>
+	return (
+		<AppBar
+			position="fixed"
+			sx={{ backgroundColor: "#7FFFD4", color: "#000000" }}
+		>
+			<Toolbar>
+				<Typography
+					variant="h6"
+					component={Link}
+					to="/"
+					sx={{
+						flexGrow: 1,
+						textDecoration: "none",
+						color: "black",
+						fontWeight: "bold",
+					}}
+				>
+					Home
+				</Typography>
 
-        <IconButton edge="end" color="inherit" onClick={toggleDrawer(true)}>
-          <MenuIcon />
-        </IconButton>
+				<IconButton
+					edge="end"
+					color="inherit"
+					onClick={toggleDrawer(true)}
+				>
+					<MenuIcon />
+				</IconButton>
 
-        <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
-          <List sx={{ width: 200 }}>
-            {["Crew", "Missions", "About"].map((text) => (
-              <ListItem button key={text} component={Link} to={`/${text.toLowerCase()}`} onClick={toggleDrawer(false)}>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-        </Drawer>
-      </Toolbar>
-    </AppBar>
-  );
+				<Drawer
+					anchor="right"
+					open={open}
+					onClose={toggleDrawer(false)}
+				>
+					<List sx={{ width: 200 }}>
+						{["Crew", "Missions", "About"].map((text) => (
+							<ListItem
+								button
+								key={text}
+								component={Link}
+								to={`/${text.toLowerCase()}`}
+								onClick={toggleDrawer(false)}
+							>
+								<ListItemText primary={text} />
+							</ListItem>
+						))}
+					</List>
+				</Drawer>
+			</Toolbar>
+		</AppBar>
+	);
 };
 
 export default NavBar;
