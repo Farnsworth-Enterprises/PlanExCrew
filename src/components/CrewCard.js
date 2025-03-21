25c215f785a39c5ad4bad3ce66cc136627a10c0c
Jordan <99503935+jbiehl88@users.noreply.github.com>
2025-03-21 15:55:09 -0500

    added some styling to Game.js and adjustments to CrewCard

--- src/components/CrewCard.js
+++ src/components/CrewCard.js
@@ -11,72 +11,58 @@ const CrewCard = ({ character, additionalInfo }) => {
 				width: "100%",
 				alignItems: "center",
 				border: "1px solid #ccc",
+				borderRadius: "12px",
+				boxShadow: "3px 3px 10px rgba(0, 0, 0, 0.1)",
+				padding: 2, 
 			}}
 		>
+			<Typography
+				variant="h6"
+				sx={{
+					textAlign: "center",
+					fontWeight: "bold",
+					marginBottom: "8px",
+				}}
+			>
+				{character.name}
+			</Typography>
+
+			{character.image && (
+				<img
+					src={character.image}
+					alt={character.name}
+					style={{
+						width: "120px",
+						height: "120px",
+						objectFit: "cover",
+						objectPosition: "top",
+						borderRadius: "8px",
+						marginBottom: "8px",
+					}}
+				/>
+			)}
+
 			<CardContent
 				sx={{
 					flexGrow: 1,
 					display: "flex",
 					flexDirection: "column",
 					alignItems: "center",
+					justifyContent: "space-between",
+					paddingBottom: 3, 
 				}}
 			>
-				<Typography
-					variant="h6"
-					sx={{
-						minHeight: "48px",
-						display: "flex",
-						alignItems: "center",
-						textAlign: "center",
-						fontWeight: "bold",
-					}}
-				>
-					{character.name}
-					{character.image && (
-						<img
-							src={character.image}
-							alt={character.name}
-							style={{
-								width: "120px",
-								height: "120px",
-								objectFit: "cover",
-								objectPosition: "top",
-								borderRadius: "8px",
-								margin: "10px 0",
-							}}
-						/>
-					)}
-					{additionalInfo[character.name] && (
-						<>
-							<Typography
-								variant="subtitle1"
-								sx={{ fontWeight: "bold" }}
-							>
-								Rating: {additionalInfo[character.name].rating}
-							</Typography>
-							<div
-								style={{
-									textAlign: "left",
-									marginTop: "8px",
-								}}
-							>
-								{/* {additionalInfo[character.name].details.map(
-									(point, index) => (
-										<Typography
-											key={index}
-											variant="body2"
-											sx={{
-												marginTop: "4px",
-											}}
-										>
-											{point}
-										</Typography>
-									)
-								)} */}
-							</div>
-						</>
-					)}
-				</Typography>
+				{additionalInfo[character.name] && (
+					<Typography
+						variant="subtitle1"
+						sx={{
+							fontWeight: "bold",
+							marginTop: "auto",
+						}}
+					>
+						Rating: {additionalInfo[character.name].rating}
+					</Typography>
+				)}
 			</CardContent>
 		</Card>
 	);
